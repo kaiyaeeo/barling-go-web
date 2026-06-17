26031374b76c535b37fdda1798f0c171bcb3bc91
kaiyaeeo <kaiyaeeo@gmail.com>
2026-06-18 03:00:53 +0700

    feat: Add initial implementation of UmkmDetailPage component

---
+++ app/(public)/umkm/[slug]/page.tsx
@@ -0,0 +1,7 @@
+    export default function UmkmDetailPage() {
+    return (
+        <div className="p-10 text-center">
+        <h1>Halaman Detail UMKM Barling-GO (Segera Hadir)</h1>
+        </div>
+    )
+    }
